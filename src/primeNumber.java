0c2bdc997901450c4c4ac369300db82a4eb809f6
Kadir Bugra <kadirbugrat@gmail.com>
2023-07-19 20:13:30 +0300

    week-2

---
+++ src/primeNumber.java
@@ -0,0 +1,25 @@
+public class primeNumber {
+    public static void main(String[] args) {
+
+        System.out.println("1 - 100 arasindaki asal sayilar:");
+
+        for (int number = 2; number <= 100; number++) {
+            if (isPrime(number)) {
+                System.out.print(number + " ");
+            }
+        }
+    }
+
+    public static boolean isPrime(int num) {
+        if (num < 2) {
+            return false;
+        }
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+}
